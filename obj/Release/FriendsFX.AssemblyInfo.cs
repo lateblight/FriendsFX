ea2278474f7e8927205057132195ba5a128922ee
BuildTools <unconfigured@null.spigotmc.org>
2026-08-28 22:28:31 +0100

    Fix release zip path structure for v1.1.0.0

--- obj/Release/FriendsFX.AssemblyInfo.cs
+++ obj/Release/FriendsFX.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyDescriptionAttribute(("Automatically manages battle effect visibilities for friends anywhere in the worl" +
     "d."))]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.1.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0.0+664f1a3a7ad32d3c14d176dc919c1ff9e35dc161")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.1.0.0+ec8cbb2a4602d0b4a089cd074c868d716ec33589")]
 [assembly: System.Reflection.AssemblyProductAttribute("FriendsFX")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FriendsFX")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.1.0.0")]
